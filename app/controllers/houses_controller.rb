9c924fdb7b608cfbcd2b6e56a1d6beabe2984b32
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-21 12:36:21 +1000

    Houses Controller crud

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,36 @@
+class HousesController < ApplicationController
+  before_action :set_house, except: %i[index create]
+
+  def index
+    render json: House.all.to_json
+  end
+
+  def show
+    render json: {
+      property: @house.to_json,
+      tenants: @house.tenants.to_json(except: [:password_digest])
+    }
+  end
+
+  def create
+    render json: House.create(house_params)
+  end
+
+  def update
+    @house.update(house_params)
+  end
+
+  def destroy
+    @house.destroy
+  end
+
+  private
+
+  def set_house
+    @house = House.find(params[:id])
+  end
+
+  def house_params
+    params.permit(:address, :title)
+  end
+end
