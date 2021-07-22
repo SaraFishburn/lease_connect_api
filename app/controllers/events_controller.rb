8e45cc2c6e88b6c9e51f17a0dad60225a2bfa4f0
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-22 12:11:21 +1000

    started crud for events, index action created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,35 @@
 class EventsController < ApplicationController
   def index
-    
+    events = houses.map(&:events)
+    render json: events.to_json
+
   end
 
-  def create
-  end
+  def create; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def houses
+    role = current_user.role_name
+
+    case role
+    when 'tenant'
+      [current_user.house]
+    when 'property manager'
+      current_user.properties
+    end
   end
 end
+
+# find current user
+
+# if current user has role 'tenant'
+#   find the house
+# else if current user has role 'property manager'
+#   find the list of pinned houses
+
+# iterate over found houses and retrieve all events asociated
+
+# send events to front end
