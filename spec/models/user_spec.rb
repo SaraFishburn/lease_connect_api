19922f6af1bfad50bb6fa867dd125ae75f7948ec
rafaelferreiramello <rafael.ferreira.sobral@gmail.com>
2021-07-26 18:54:14 +1000

    Testing Rails test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#     it "User invalid" do
+#       model = User.create
+#       expect(model).to.not be_valid
+#     end
+# end
+
+# RSpec.describe User, type: :model do
+#   it "Check routes" do
+
+
+#   end
+# end
+
+# RSpec.describe User, type: :model do
+#   it "Check API" do
+
+
+#   end
+# end
--- spec/user_rspec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, :type :model do
-    model = User.create
-    expect(model).to be_valid
-
-end
