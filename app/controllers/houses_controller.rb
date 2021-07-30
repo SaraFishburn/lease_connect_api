a6a118aeb716e2e911a21e54451ee4ad92435c81
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-30 10:40:55 +1000

    added error response on delete action for house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -30,7 +30,7 @@ class HousesController < ApplicationController
   end
 
   def destroy
-    return render json: { error: 'tenants must be removed before deleting house' } unless @house.tenants.count.zero?
+    return render json: { error: 'ERROR: tenants must be removed before deleting house' }, status: 401 unless @house.tenants.count.zero?
 
     @house.destroy
   end
