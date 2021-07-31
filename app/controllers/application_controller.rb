f19324bde3af07eeafa078fb6affff4a0ac80caa
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-31 13:46:38 +1000

    final commit FULLY DEPLOYED YESSSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::API
   # method to get the jwt token and decrypt and retrieve the json hash
   def jwt_data
     token = request.headers['Authorization'].split(' ')[1]
-    puts token
-    puts "SECRET KEY BASE"
-    puts Rails.application.secret_key_base
-    puts "^^^^^^^^^"
 
     # create new hash containing json data
     begin
@@ -15,37 +11,26 @@ class ApplicationController < ActionController::API
         # retrieve json data from decrypted info
         JWT.decode(token, Rails.application.secret_key_base, true)[0]
       )
-    rescue JWT::DecodeError => e
-      puts e
-      puts "decode error"
+    rescue JWT::DecodeError
       nil
     end
   end
 
   def current_user
-    puts '---------------------------------'
-    puts 'CURRENT USER1'
-    puts @current_user
     # return User.first
     # Check if the current_user has already been calculated
     return @current_user if @current_user
 
-    puts "2"
-    
     # Get the data from the jwt_cookie function
     data = jwt_data
-    
+
     # If there is no data user isn't logged in so return nil
     return nil unless data
-    
-    puts "3"
+
     # Find the user and save it in an instance variable for later
     # (value is also returned)
     @current_user = User.find(data[:id])
 
-    puts '---------------------------------'
-    puts 'CURRENT USER4'
-    puts @current_user
     @current_user
   end
 
