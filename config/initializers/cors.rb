bb010a2607d7fb5f68b1764f8576b5709c639996
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-31 12:33:19 +1000

    fixing for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins ENV['LOCAL_FRONT_END']
-    origins '*'
+    origins ENV['LOCAL_FRONT_END']
 
     resource '*',
       headers: :any,
