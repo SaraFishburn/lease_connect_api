c889d4a7ba47387ddc9d319d2dee1be2cfd8cc35
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-31 13:06:14 +1000

    printing the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,23 +17,27 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
+    puts '---------------------------------'
+    puts 'CURRENT USER'
+    puts @current_user
     # return User.first
     # Check if the current_user has already been calculated
     return @current_user if @current_user
 
     # Get the data from the jwt_cookie function
     data = jwt_data
-
+    
     # If there is no data user isn't logged in so return nil
     return nil unless data
-
+    
     # Find the user and save it in an instance variable for later
     # (value is also returned)
     @current_user = User.find(data[:id])
-
+    
     puts '---------------------------------'
     puts 'CURRENT USER'
-    puts current_user
+    puts @current_user
+    @current_user
   end
 
   # Method to revoke access to controller actions to unauthorized users
